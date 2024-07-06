63ac6c92fa894cd3af2a91fde82bb5c6d211365d
Bekbol <19kani92@gmail.com>
2024-07-06 15:24:26 +0600

    сделал миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -8,7 +8,7 @@ class GuestBook(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False, verbose_name='Почта')
     text = models.TextField(max_length=500, null=False, blank=False, verbose_name='Текст записи')
     creation_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    update_time = models.DateTimeField(auto_now_add=True, verbose_name='Время редактирования')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='Время редактирования')
     status = models.CharField(max_length=100, null=False, blank=False, choices=status_choices,
                               default='active', verbose_name='Статус')
 
