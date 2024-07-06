635f70ddbe6ef756a5947af2e7af8ba2ca017e42
Bekbol <19kani92@gmail.com>
2024-07-06 11:42:13 +0600

    сделал модельку, создал админку, сделал миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import GuestBook
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'text', 'id', 'status']
+    list_filter = ['name', 'email', 'status', 'id']
+    list_display_links = ['text', 'name']
+    search_fields = ['name', 'status']
+    fields = ['name', 'email', 'text', 'id']
+    readonly_fields = ['creation_time', 'update_time']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+status_choices = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+
+class GuestBook(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Имя')
+    email = models.EmailField(max_length=254, null=False, blank=False, verbose_name='Почта')
+    text = models.TextField(max_length=500, null=False, blank=False)
+    creation_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    update_time = models.DateTimeField(auto_now_add=True, verbose_name='Время редактирования')
+    status = models.CharField(max_length=100, null=False, blank=False, choices=status_choices,
+                              default='active', verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.name} {self.email} {self.text} {self.status}'
+
+    class Meta:
+        db_table = 'GuestBooks'
+        verbose_name = 'Гостевая книга'
+        verbose_name_plural = 'Гостевые книги'
