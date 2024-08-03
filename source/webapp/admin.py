04ff2d791205a4b714cef1f7d73b8b3c6d484033
Bekbol <19kani92@gmail.com>
2024-08-03 17:44:32 +0600

    не те скобки использовал

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -3,15 +3,15 @@ from .models import Topic, Answer
 
 
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'user', 'get_reply_count')
-    search_fields = ('title', 'content')
-    list_filter = ('created_at',)
+    list_display = ['title', 'created_at', 'user']
+    search_fields = ['title', 'content']
+    list_filter = ['created_at',]
 
 
 class ReplyAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'created_at', 'topic')
-    search_fields = ('content',)
-    list_filter = ('created_at', 'topic')
+    list_display = ['user', 'content', 'created_at', 'topic']
+    search_fields = ['content',]
+    list_filter = ['created_at', 'topic']
 
 
 admin.site.register(Topic, TopicAdmin)
