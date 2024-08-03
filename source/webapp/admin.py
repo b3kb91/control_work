cb54e39a3a82d928c36a0e57286b3777544dabdd
Bekbol <19kani92@gmail.com>
2024-08-03 17:43:04 +0600

    создал админку

---
+++ source/webapp/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Topic, Answer
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'user', 'get_reply_count')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at',)
+
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'created_at', 'topic')
+    search_fields = ('content',)
+    list_filter = ('created_at', 'topic')
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Answer, ReplyAdmin)
